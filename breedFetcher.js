5e3424e0b477b0a5520b240f943b3260ef08cb8d
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-18 18:23:30 -0400

    eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,16 +6,18 @@ const getRequest = {
   url: 'https://api.thecatapi.com/v1/breeds/search?q=sib',
   method: 'GET',
 };
-console.log(typeof body)
+console.log(typeof body);
 
-const data = JSON.parse(body);
-console.log(data);
-console.log(typeof data);
 
-request(getRequest, (error, response, body) => {
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'
+request(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
-})
+  const data = JSON.parse(body);
+console.log(data);
+console.log(typeof data);
+
+});
 
