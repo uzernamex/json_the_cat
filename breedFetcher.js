02f5b341f9d8b5a84797f7dd1d71cbbbf60f824b
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-18 18:48:24 -0400

    added test files to code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,15 @@
 const request = require('./request');
-
-const getRequest = {
-  hostname: 'www.thecatapi.com',
-  port: 3000,
-  url: 'https://api.thecatapi.com/v1/breeds/search?q=sib',
-  method: 'GET',
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+  });
+  const data = JSON.parse(request);
+  console.log(data);
+  console.log(typeof data);
 };
-console.log(typeof body);
 
-
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-  const data = JSON.parse(body);
-console.log(data);
-console.log(typeof data);
-
-});
-
+module.exports = { fetchBreedDescription };
