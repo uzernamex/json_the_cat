7b931304799beefb26c995abf150739afaaf72f9
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-18 17:34:15 -0400

    error handling added to code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,21 @@
 const request = require('./request');
+
+const getRequest = {
+  hostname: 'www.thecatapi.com',
+  port: 3000,
+  url: 'https://api.thecatapi.com/v1/breeds/search?q=sib',
+  method: 'GET',
+};
+console.log(typeof body)
+
+const data = JSON.parse(body);
+console.log(data);
+console.log(typeof data);
+
+request(getRequest, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+})
+
